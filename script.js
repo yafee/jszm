7b877e967cdcb712ac4054e856fd3a92fd1efc36
ya_fee <ya_fee@163.com>
2016-09-08 20:31:37 +0800

    s
    s

--- script.js
+++ script.js
@@ -10,13 +10,21 @@ $(function() {
   $('.reTop').on('click', function() {
     $('body,html').animate({
       scrollTop: 0
-    }, 500)
-  })
+    }, 500);
+  });
   var navList = $('.menu li');
   $('.nav-toggle').on('click', function() {
     $('.menu').slideToggle();
   });
   navList.on('click', function() {
     $('.menu').slideToggle();
-  })
+  });
+  stepImgVC();
+
+  // 服务流程图片垂直居中
+  function stepImgVC() {
+    var stepImgHg = $('.stepImg').parent().css('height');
+    $('.stepImg').css('line-height', stepImgHg);
+    console.log(stepImgHg);
+  }
 });
