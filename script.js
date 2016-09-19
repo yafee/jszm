3851cd7d24923aedd77137d6e20628beea22480f
ya_fee <ya_fee@163.com>
2016-09-19 14:17:33 +0800

    s
    s

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ $(function() {
   $('.main>div[id]').each(function() {
     hgArr.push($(this).offset().top);
   });
-  $('.navList li,.menu li').on('click', function() {
+  $('.navList li,.menu li,.footer li.link').on('click', function() {
     var index = $(this).index();
-    // $(window).scrollTop(hgArr[index] - navBarHg);
-    $(window).animate({scrollTop:hgArr[index] - navBarHg}, 1000);
+    //底部导航栏
+    if($(this).hasClass('link')&&index>2){
+      index = index-1;
+    }
+    $('body,html').animate({scrollTop:hgArr[index] - navBarHg}, 500);
     if (!$(this).hasClass('inline')) {
-      $('.menu').slideToggle();
+      $('.menu').toggle();
     }
   });
   //返回顶部
@@ -28,9 +31,11 @@ $(function() {
     }, 500);
   });
   $('.nav-toggle').on('click', function() {
-    $('.menu').slideToggle();
+    $('.menu').toggle();    
   });
   stepImgVC();
+
+  //合作伙伴滚动
 
   // 服务流程图片垂直居中
   function stepImgVC() {
